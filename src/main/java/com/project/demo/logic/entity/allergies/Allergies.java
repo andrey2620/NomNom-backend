3e18fbac054bf17469a7ff3ca4b7cb82b7a1acf7
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-04-08 14:05:31 -0600

    bug el json no cargaba-arreglado

--- src/main/java/com/project/demo/logic/entity/allergies/Allergies.java
+++ src/main/java/com/project/demo/logic/entity/allergies/Allergies.java
@@ -1,14 +1,18 @@
 package com.project.demo.logic.entity.allergies;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.project.demo.logic.entity.user.User;
 import jakarta.persistence.*;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Table(name = "allergies")
 public class Allergies {
 
-    @Id
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_allergies")
     private Long id;
 
+  @Column(name = "name", length = 100, nullable = false, unique = true)
     private String name;
 
 
