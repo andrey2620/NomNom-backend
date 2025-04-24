eb432256ca7a05c404bd61d10d46bbab0db78251
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-04-24 17:47:34 -0600

    shoopingList-crea lista manual funcional

--- src/main/java/com/project/demo/logic/entity/ShoppingList/ShoppingListItem.java
+++ src/main/java/com/project/demo/logic/entity/ShoppingList/ShoppingListItem.java
@@ -3,6 +3,7 @@ package com.project.demo.logic.entity.ShoppingList;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.project.demo.logic.entity.ingredient.Ingredient;
 import jakarta.persistence.*;
+import org.springframework.lang.Nullable;
 
 import java.math.BigDecimal;
 
@@ -14,14 +15,20 @@ public class ShoppingListItem {
   @Column(name = "id_shoppingList_item")
   private Long id;
 
+  @Column(name = "custom_name")
+  private String customName;
+
+  @Column(name = "custom_quantity")
+  private String customQuantity;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "id_shopping_list", nullable = false)
   @JsonBackReference
   private ShoppingList shoppingList;
 
+  @Nullable
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "id_ingredient", nullable = false)
+  @JoinColumn(name = "id_ingredient", nullable = true)
   private Ingredient ingredient;
 
   @Column(nullable = false, precision = 10, scale = 2)
@@ -71,5 +78,21 @@ public class ShoppingListItem {
 
   public void setShoppingList(ShoppingList shoppingList) {
     this.shoppingList = shoppingList;
+  }
+
+  public String getCustomName() {
+    return customName;
+  }
+
+  public void setCustomName(String customName) {
+    this.customName = customName;
+  }
+
+  public String getCustomQuantity() {
+    return customQuantity;
+  }
+
+  public void setCustomQuantity(String customQuantity) {
+    this.customQuantity = customQuantity;
   }
 }
